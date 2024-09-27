8f5fe263588d492c1831ce91df9be6969337d6c1
Nadolina <nadibraj@gmail.com>
2024-09-27 12:17:31 -0400

    small edit, realized not all output directories were changed correctly, corrected now

--- bcftools.sh
+++ bcftools.sh
@@ -62,7 +62,7 @@ do
 		-a AD,DP,SP \
 		-o bcftools_${rundate}/mpileup-${chrnum}-${rundate}.vcf.gz \
 		-Oz --threads 8 \
-		2> ${PWD}/bcftools_logs/mpileup-${chrnum}-${rundate}-\${SLURM_JOB_ID}.log; bcftools index -t --threads 8 -o bcftools_out/mpileup-${chrnum}-${rundate}.vcf.gz.tbi bcftools_out/mpileup-${chrnum}-${rundate}.vcf.gz\n" >> mpileup-${rundate}.swarm
+		2> ${PWD}/bcftools_logs/mpileup-${chrnum}-${rundate}-\${SLURM_JOB_ID}.log; bcftools index -t --threads 8 -o bcftools_${rundate}/mpileup-${chrnum}-${rundate}.vcf.gz.tbi bcftools_${rundate}/mpileup-${chrnum}-${rundate}.vcf.gz\n" >> mpileup-${rundate}.swarm
 	printf "bcftools annotate -a $thousandgAF \
 		-h $thousandgHDR \
 		-c CHROM,POS,REF,ALT,REF_AN,REF_AC \
