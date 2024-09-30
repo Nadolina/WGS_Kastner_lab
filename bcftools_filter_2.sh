b37ca89a8eb0e9677b53ea4b56f2a7be2df36ff1
Nadolina <nadibraj@gmail.com>
2024-09-30 15:47:20 -0400

    few more changes to output directories and such for bcftools filtering

--- bcftools_filter_2.sh
+++ bcftools_filter_2.sh
@@ -24,21 +24,22 @@ done
 module load R/4.4.1
 module load bcftools/1.19
 module load picard/3.2.0
-# module load vcflib
+module load vcflib
 # module load vcftools
 
 rundate=`date +'%m%d%y'`
 cd ${vcfdir}
+printf "Working in ${vcfdir}.\n" 
 
 ## merging the VCF and getting the number of variants identified across all chromosomes and samples 
-vcflist=`ls ${vcfdir} | grep '.vcf.gz$' | grep '^call'| sort -V`
+vcflist=`ls | grep '.vcf.gz$' | grep '^call'| sort -V`
 
 mergevcf=merged-bcftools-$rundate.vcf.gz
 
 gatherinput=""
 for vcf in ${vcflist};
 do
-    gatherinput+="I=${vcfdir}/${vcf} " 
+    gatherinput+="I=${vcf} " 
 done 
 
 echo "java -jar $PICARDJAR GatherVcfs ${gatherinput} 0=$mergevcf" 2> gathervcfs-bcftools-${rundate}.log
@@ -49,7 +50,7 @@ bcftools index -t $mergevcf
 samples=`bcftools query -l $mergevcf`
 samplestr=`echo $samples | sed -E 's/\n/,/g'`
 
-printf "The number of variants in %s is: " "$samplestr" >> gathervcfs-bcftools-$rundate.log ; bcftools view -H $mergevcf | wc -l >> gathervcfs-bcftools-$rundate.log 
+#printf "The number of variants in %s is: " "$samplestr" >> gathervcfs-bcftools-$rundate.log ; bcftools view -H $mergevcf | wc -l >> gathervcfs-bcftools-$rundate.log 
 
 # Random sampling the VCF to create a subset we can identify filtering thresholds from 
 
@@ -68,7 +69,7 @@ out="stats/snp-out"
 bcftools query -f '%CHROM\t%POS\t%QUAL\t%DP\t%MQBZ' subset-bcftools-snps-$rundate.vcf.gz -o snp-metrics-$rundate.tsv 
 bcftools query -f '%CHROM\t%POS\t%QUAL\t%DP\t%IMF' subset-bcftools-indels-$rundate.vcf.gz -o indels-metrics-$rundate.tsv 
 
-Rscript -e "rmarkdown::render('/data/brajukanm/scripts/bcftools_filter_thresholds_2.rmd',params=list(myargs = '${PWD}'),output_dir='${PWD}')"
+Rscript -e "rmarkdown::render('/data/Kastner_PFS/scripts/pipelines/WGS_Kastner_lab/bcftools_filter_thresholds_2.rmd',params=list(myargs = '${PWD}'),output_dir='${PWD}')"
 
 qual=`cut -f2 -d ',' parameters_snps.csv| awk 'NR==2'`
 mindepth=`cut -f2 -d ',' parameters_snps.csv| awk 'NR==3'`
